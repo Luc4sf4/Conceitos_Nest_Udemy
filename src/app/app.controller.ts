4617ccc87e366034773732dd868213f20c5771d3
Luc4sf4 <superlucas2005@gmail.com>
2025-04-15 10:09:56 -0300

    Basico sobre protocolo HHTP, URL, recursos e metodos de solicitacao (GET)

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+// import { AppService } from './app.service';
 
-@Controller()
+@Controller('home')//Decorator definindo a classe como uma controller, podendo ate mesmo definir os recursos dentro do Decorator
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  // constructor(private readonly appService: AppService) {}
 
-  @Get()
+  //Metodo da solicitacao -> Ler (Read) -> cRud
+  //podemos definir o outro recurso(ou URL, ja que cada Decorator de route deixa uma "/" caso esteja vazio) dentro desse decorator
+  //ficaria -> /home/hello
+  @Get('hello')
   getHello(): string {
-    return this.appService.getHello();
+    return "Qualquer Coisa";
   }
 }
