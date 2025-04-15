b96628bf569129c9a365f9ed616d340b83cf2598
Luc4sf4 <superlucas2005@gmail.com>
2025-04-15 09:51:07 -0300

    Primeiro commit, Criando meu proprio Module manualmente ou com Nest CLI

---
+++ src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConceitosManualModule } from 'src/conceitos-manual/conceitos-manual.module';
+import { ConceitosAutomaticoModule } from 'src/conceitos-automatico/conceitos-automatico.module';
+
+@Module({
+  imports: [ConceitosManualModule, ConceitosAutomaticoModule],
+  controllers: [AppController],//-> quem controla os request e as responses, onde pode ter as services utilizados para aplicar a logica de negocios
+  providers: [AppService],// -> usado para injetar dependencias
+  exports:[]
+})
+export class AppModule {}
---
+++ src/conceitos-manual/conceitos-manual.module.ts
@@ -0,0 +1,7 @@
+import { Module } from "@nestjs/common";
+//Maioria das classes precisa de um Decorator que seriam basicamente as anotacoes de cada classe, mudando o comportamento da classe, e geralmente
+//fica em cima dos metodos
+@Module({
+
+})
+export class ConceitosManualModule{}
