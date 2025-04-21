de40657ff79f59df26c47d8e3d8ab30dfdbd2502
Luc4sf4 <superlucas2005@gmail.com>
2025-04-21 13:33:40 -0300

    Configurando o  TypeORM

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RecadosModule } from 'src/recados/recados.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [RecadosModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      database: 'postgres',
+      password: 'admin',
+      autoLoadEntities: true, // carrega entidades sem precisar especifica-las
+      synchronize: true, //sincroniza com o BD. nao deve ser usado em produção
+    }),
+    RecadosModule,
+  ],
   controllers: [AppController], //-> quem controla os request e as responses
   providers: [AppService], // -> usado para injetar dependências  onde pode ter as services utilizados para aplicar a logica de negócios
   exports: [],
