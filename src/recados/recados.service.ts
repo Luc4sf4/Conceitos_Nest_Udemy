f0190fa56012935e91c7fe30263076fd5a8b6c07
Luc4sf4 <superlucas2005@gmail.com>
2025-04-21 15:07:03 -0300

    Usando o Repository para ataulizar uma entidade na tabela

--- src/recados/recados.service.ts
+++ src/recados/recados.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/await-thenable */
 import { CreateRecadoDto } from './dto/create-recado.dto';
 
 import { Injectable, NotFoundException } from '@nestjs/common';
@@ -14,24 +15,14 @@ Operações com banco de dados, usando https/Apis externas, Delay, timeouts e et
 
 @Injectable()
 export class RecadosService {
+  //Cria o construtor
   constructor(
-    //Cria o construtor
-    @InjectRepository(Recado) //injeção de dependência do repositório falando a entidade
-    private readonly recadoRepository: Repository<Recado>, //cira o recadoRepository passando o tipo Repository do TypeOrm passando o Recado da Entidade
+    //injeção de dependência do repositório falando a entidade
+    @InjectRepository(Recado)
+    //cira o recadoRepository passando o tipo Repository do TypeOrm passando
+    //o Recado da Entidade
+    private readonly recadoRepository: Repository<Recado>,
   ) {}
-
-  private lastId = 1;
-  private recados: Recado[] = [
-    {
-      id: 1,
-      texto: 'Este e um recado teste',
-      de: 'Joana',
-      para: 'João',
-      lido: false,
-      data: new Date(),
-    },
-  ];
-
   throwNotFundError() {
     throw new NotFoundException('Recado nao encontrado');
   }
@@ -44,8 +35,8 @@ export class RecadosService {
     significa que ele pode esperar pela resolução de Promises (como chamadas de
     banco de dados, APIs externas, etc), sem bloquear a execução do restante do
     código.
-    Em resumo, usamos `await` para esperar que a Promise com os dados seja resolvida
-    antes de retornar o resultado.
+    Em resumo, usamos `await` para esperar que a Promise com os dados seja 
+    resolvida antes de retornar o resultado.
   */
   async findAll() {
     const recados = await this.recadoRepository.find();
@@ -61,46 +52,39 @@ export class RecadosService {
     if (recado) return recado;
     this.throwNotFundError();
   }
-  create(createRecadoDto: CreateRecadoDto) {
-    this.lastId++; //adiciona um numero da sequencia de id
-    const id = this.lastId; //relaciona a constante id com o lastId
+  async create(createRecadoDto: CreateRecadoDto) {
     const novoRecado = {
-      id,
       ...createRecadoDto,
       lido: false,
       data: new Date(),
     }; //recebe o json desejado
-    this.recados.push(novoRecado); //adiciona no array
-
-    return novoRecado; //retorna mostrando o JSON
+    //adiciona no array
+    const recado = await this.recadoRepository.create(novoRecado);
+    return this.recadoRepository.save(recado); //retorna mostrando o JSON
   }
 
-  update(id: string, updateRecadoDto: UpdateRecadoDto) {
-    const recadoExistenteIndex = this.recados.findIndex(
-      item => item.id === +id, //acha o index do recado que estamos tentando apagar
-    );
-    if (recadoExistenteIndex < 0) {
-      this.throwNotFundError();
-    }
-    const recadoExistente = this.recados[recadoExistenteIndex]; // encontra o recado que estamos procurando
-    this.recados[recadoExistenteIndex] = {
-      ...recadoExistente,
-      ...updateRecadoDto,
-    }; //troca as chaves do recado existente para o recado que desejamos atualizar
+  async update(id: number, updateRecadoDto: UpdateRecadoDto) {
+    const partialUpdateRecadoDto = {
+      lido: updateRecadoDto?.lido,
+      text: updateRecadoDto?.texto,
+    }; //utilizando para selecionar quais campos em especifico que
+    //podemos alterar
+    const recado = await this.recadoRepository.preload({
+      //pre carrega os campos que iremos atualizar pegando ele com as
+      // atualizações que iremos receber
+      id,
+      ...partialUpdateRecadoDto,
+    });
+    if (!recado) return this.throwNotFundError(); //se recado nao existir
+    return this.recadoRepository.save(recado);
   }
-  remove(id: number) {
-    const recadoExistenteIndex = this.recados.findIndex(
-      item => item.id === id, //acha o index do recado que estamos tentando apagar
-    );
+  async remove(id: number) {
+    const recado = await this.recadoRepository.findOneBy({
+      id,
+    });
 
-    if (recadoExistenteIndex < 0) {
-      this.throwNotFundError();
-    }
-
-    const recado = this.recados[recadoExistenteIndex];
-
-    this.recados.splice(recadoExistenteIndex, 1); //apaga o index da array
-
+    if (!recado) return this.throwNotFundError();
+    await this.recadoRepository.remove(recado);
     return recado;
   }
 }
