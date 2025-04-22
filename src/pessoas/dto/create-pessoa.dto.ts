38625cad1e645ade343b253c8d6046901bfba694
Luc4sf4 <superlucas2005@gmail.com>
2025-04-21 22:24:47 -0300

    Crud: create de pessoa com email sendo unico no bd

--- src/pessoas/dto/create-pessoa.dto.ts
+++ src/pessoas/dto/create-pessoa.dto.ts
@@ -1,1 +1,23 @@
-export class CreatePessoaDto {}
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class CreatePessoaDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  @MaxLength(100)
+  readonly name?: string;
+
+  @IsEmail()
+  readonly email?: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(5)
+  readonly senha?: string;
+}
--- src/pessoas/pessoas.service.ts
+++ src/pessoas/pessoas.service.ts
@@ -1,13 +1,35 @@
-import { Injectable } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+import { Pessoa } from './entities/pessoa.entity';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { CreatePessoaDto } from './dto/create-pessoa.dto';
 import { UpdatePessoaDto } from './dto/update-pessoa.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class PessoasService {
-  create(createPessoaDto: CreatePessoaDto) {
-    return 'This action adds a new pessoa';
-  }
+  constructor(
+    @InjectRepository(Pessoa)
+    private readonly pessoaRepository: Repository<Pessoa>,
+  ) {}
 
+  async create(createPessoaDto: CreatePessoaDto) {
+    try{ 
+      const novaPessoa = {
+        nome: createPessoaDto.name,
+        passwordHash: createPessoaDto.senha,
+        email: createPessoaDto.email,
+      };
+      const pessoa = this.pessoaRepository.create(novaPessoa); //cria a pessoa
+      await this.pessoaRepository.save(pessoa); //salva a pessoa no banco de dados
+      return pessoa;
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('email ja cadastrado');
+      }
+      throw error;
+    }
+  }
   findAll() {
     return `This action returns all pessoas`;
   }
