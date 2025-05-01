520397cb877beea0a13e51d5116265a218c5047a
Luc4sf4 <superlucas2005@gmail.com>
2025-05-01 16:47:21 -0300

    Instalando @nestJs/config e criando as variaveis de ambiente (.env)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,19 @@ import { AppService } from './app.service';
 import { RecadosModule } from 'src/recados/recados.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PessoasModule } from 'src/pessoas/pessoas.module';
+import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
+    ConfigModule.forRoot(), // por padrão carrega o arquivo .env
     TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      database: 'postgres',
-      password: 'admin',
-      autoLoadEntities: true, // carrega entidades sem precisar especifica-las
-      synchronize: true, //sincroniza com o BD. nao deve ser usado em produção
+      type: process.env.DATABASE_TYPE as 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT || '5432', 10),
+      username: process.env.DATABASE_USERNAME,
+      database: process.env.DATABASE_DATABASE,
+      password: process.env.DATABASE_PASSWORD,
+      autoLoadEntities: Boolean(process.env.DATABASE_AUTOLOADENTITIES), // carrega entidades sem precisar especifica-las
+      synchronize: Boolean(process.env.DATABASE_SYNCHRONIZE), //sincroniza com o BD. nao deve ser usado em produção
     }),
     RecadosModule,
     PessoasModule,
