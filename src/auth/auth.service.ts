15835c5655178b922e45608fd1aa8b9a0bf2bb2d
Luc4sf4 <superlucas2005@gmail.com>
2025-05-19 14:48:34 -0300

    Criando AuthController e AuthService

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/require-await */
+import { Injectable } from '@nestjs/common';
+import { LoginDto } from './dto/login.dto';
+
+@Injectable()
+export class AuthService {
+  async login(loginDto: LoginDto) {
+    return loginDto;
+  }
+}
