519077eae864134306e657121ec223f2cea969d2
Luc4sf4 <superlucas2005@gmail.com>
2025-05-19 15:02:34 -0300

    Autenticando o usuario com email e senha

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,40 @@
-/* eslint-disable @typescript-eslint/require-await */
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { LoginDto } from './dto/login.dto';
+import { Repository } from 'typeorm';
+import { Pessoa } from 'src/pessoas/entities/pessoa.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { HashingService } from './hashing/hashing.service';
 
 @Injectable()
 export class AuthService {
+  constructor(
+    @InjectRepository(Pessoa)
+    private readonly pessoaRepository: Repository<Pessoa>,
+    private readonly hashingService: HashingService,
+  ) {}
+
   async login(loginDto: LoginDto) {
-    return loginDto;
+    const pessoa = await this.pessoaRepository.findOneBy({
+      email: loginDto.email,
+    });
+
+    if (!pessoa) {
+      throw new UnauthorizedException('Pessoa nao existe.');
+    }
+
+    const passwordIsValid = await this.hashingService.compare(
+      loginDto.password,
+      pessoa.passwordHash,
+    );
+
+    if (!passwordIsValid) {
+      throw new UnauthorizedException('Senha invalida.');
+    }
+
+    // Fazer o novo token e entregar para o usuario na resposta
+
+    return {
+      message: 'Usuário logado!',
+    };
   }
 }
