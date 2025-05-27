37a5f308be6268ca6c22866518281cadec3036d4
Luc4sf4 <superlucas2005@gmail.com>
2025-05-27 12:00:07 -0300

    Escrevendo um teste para garantir que uma Exception ocorreu

--- src/pessoas/pessoas.service.spec.ts
+++ src/pessoas/pessoas.service.spec.ts
@@ -8,6 +8,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { CreatePessoaDto } from './dto/create-pessoa.dto';
 import { create } from 'node:domain';
+import { ConflictException } from '@nestjs/common';
 
 describe('PessoasService', () => {
   let pessoaService: PessoasService;
@@ -45,31 +46,61 @@ describe('PessoasService', () => {
 
   describe('create', () => {
     it('deve criar uma nova pessoa', async () => {
-      // CreatePessoaDto
-      //Arrange
+      //Arrange -- montando as coisas que eu vou precisar retornar la na frente
       const createpessoaDto: CreatePessoaDto = {
         email: 'lucas@email.com',
         name: 'Lucas',
         senha: '123456',
       };
       const passwordHash = 'HASHDESENHA';
-      // Que o hashing service tenha o método hash
-      // Saber se o hashing service foi chamado com o CreatePessoaDto
-      // Saber se o pessoaRepository.create foi chamado com dados pessoa
-      // Saber se o pessoaRepository.save foi chamado com a pessoa criada
-      // O retorno final deve ser a nova pessoa criada -> expect
+      const novaPessoa = {
+        id: 1,
+        nome: createpessoaDto.name,
+        passwordHash,
+        email: createpessoaDto.email,
+      };
 
-      jest.spyOn(hashingService, 'hash').mockResolvedValue('HASHDESENHA');
+      // Como o valor retornado por hashingService.service e necessário
+      //vamos simular este valor.
+      jest.spyOn(hashingService, 'hash').mockResolvedValue(passwordHash);
+
+      // Como a pessoa retornada por pessoaRepository e necessaria em
+      // pessoaRepository.save. Vamos simular este valor
+      jest.spyOn(pessoaRepository, 'create').mockReturnValue(novaPessoa as any);
+
       //Act
-      await pessoaService.create(createpessoaDto);
+      const result = await pessoaService.create(createpessoaDto);
 
+      //Assert
+      // O método hashingService.hash foi chamado com o createPessoaDto.senha ?
       expect(hashingService.hash).toHaveBeenCalledWith(createpessoaDto.senha); //.toHaveBeenCalled();
 
+      //O método pessoaRepository.create foi chamado com os dados da nova
+      // pessoa com o hash de senha criado por hashingService.hash?
       expect(pessoaRepository.create).toHaveBeenCalledWith({
         nome: createpessoaDto.name,
         passwordHash,
         email: createpessoaDto.email,
       });
+
+      // O método pessoaRepository.save foi chamado com os dados da nova
+      // pessoa gerada por pessoaRepository.create ?
+      expect(pessoaRepository.save).toHaveBeenCalledWith(novaPessoa);
+
+      // O resultado do método pessoaService.create retornou a nova pessoa
+      // criada ?
+      expect(result).toEqual(novaPessoa);
+    });
+
+    it('deve lançar um ConflictException', async () => {
+      //
+      jest.spyOn(pessoaRepository, 'save').mockRejectedValue({
+        code: '23505',
+      });
+
+      await expect(pessoaService.create({} as any)).rejects.toThrow(
+        ConflictException,
+      );
     });
   });
 });
